cf6537d0eb82828a22461b5eac6079e636a0bfff
JoyBoyIsHere-D <jayanshujubujena@gmail.com>
2024-11-03 00:09:39 +0530

    nav changes

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -16,45 +16,7 @@ function Navbar() {
             </Link>
           </div>
 
-          {/* Desktop Navigation Links */}
-          <div className="hidden md:flex space-x-8">
-            <Link to="/home" className="text-gray-700 hover:text-blue-600">
-              Home
-            </Link>
-            <Link to="/jobs" className="text-gray-700 hover:text-blue-600">
-              Jobs
-            </Link>
-            <Link to="/about" className="text-gray-700 hover:text-blue-600">
-              About
-            </Link>
-            <Link to="/contact" className="text-gray-700 hover:text-blue-600">
-              Contact
-            </Link>
-          </div>
-
-          {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              type="button"
-              className="text-gray-700 hover:text-blue-600 focus:outline-none"
-            >
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
-                />
-              </svg>
-            </button>
-          </div>
+          
         </div>
       </div>
 
